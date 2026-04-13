cf3dc9c9630efc376ea522801db73ed81195c466
Abdalkabar <aabolghed@gmail.com>
2026-04-13 19:59:50 +0300

    project 6

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
-// Declare a variable
-let myVariable = "Hello, World!";
+function Vehicle(Make, Model, Year, Color) {
+    this.Vehicle_Make = Make;
+    this.Vehicle_Model = Model;
+    this.Vehicle_Year = Year;
+    this.Vehicle_Color = Color;
+}
 
-// Use document.write() and typeof operator to display the data type
-document.write("The data type of myVariable is: " + typeof myVariable);
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
+var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
 
-// Demonstrate type coercion
-let num = 5;
-let str = "10";
-let coercedResult = num + str; // Type coercion: number + string = string
-document.write("<br>The result of type coercion (5 + '10'): " + coercedResult + " (type: " + typeof coercedResult + ")");
+function myFunction() {
+    document.getElementById("New_and_This").innerHTML =
+        "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model +
+        " manufactured in " + Erik.Vehicle_Year;
+}
 
-// Display Infinity and -Infinity
-let largeNumber = Number.MAX_VALUE * 2;
-let negativeLargeNumber = -Number.MAX_VALUE * 2;
-document.getElementById("output").innerHTML = "Infinity: " + largeNumber + "<br>-Infinity: " + negativeLargeNumber + "<br>Boolean Logic:<br>5 > 3: " + (5 > 3) + "<br>3 < 5: " + (3 < 5) + "<br>5 < 3: " + (5 < 3) + "<br>Double Equal Signs:<br>5 == '5': " + (5 == "5") + "<br>5 == 6: " + (5 == 6) + "<br>Triple Equal Signs:<br>5 === 5: " + (5 === 5) + "<br>5 === '5': " + (5 === "5") + "<br>5 === 6: " + (5 === 6) + "<br>'hello' === 'world': " + ("hello" === "world") + "<br>AND Operator:<br>true && true: " + (true && true) + "<br>true && false: " + (true && false) + "<br>OR Operator:<br>true || false: " + (true || false) + "<br>false || false: " + (false || false) + "<br>NOT Operator:<br>!true: " + (!true) + "<br>!false: " + (!false);
-
-// Perform a math operation with console.log()
-console.log("Math operation: 10 + 5 = " + (10 + 5));
-
-// Boolean Challenge: Display “false” in the console using Boolean logic
-console.log("Boolean logic resulting in false: 5 > 10 is " + (5 > 10));
+function nestedFunction() {
+    function innerFunction() {
+        return "Nested function result.";
+    }
+    document.getElementById("Nested_Function").innerHTML = innerFunction();
+}
