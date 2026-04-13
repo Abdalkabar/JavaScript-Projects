b970bed3d65aa6f8875589aa44dcb8fc1f069c91
Abdalkabar <aabolghed@gmail.com>
2026-04-13 17:12:06 +0300

    assignment on js2

--- main.js
+++ main.js
@@ -1,55 +1,13 @@
-// JavaScript Basics Demonstration
+function showCalculation() {
+    // Assign two variables
+    var number1 = 8;
+    var number2 = 12;
 
-// Section 1: Assigning a string to a variable and displaying it with alert
-// Assign a string value to a variable
-var myMessage = "Hello, this is my string variable!";
+    // Build a message string using += to concatenate
+    var resultMessage = 'The sum of ' + number1 + ' and ' + number2 + ' is ';
+    resultMessage += (number1 + number2) + '.';
 
-// Use the window.alert() method to display the variable
-window.alert(myMessage);
-
-// Section 2: Working with escaped characters in strings
-// String with apostrophe and quotation marks (escaped)
-var quoteMessage = "It's a beautiful day! He said, \"Hello, world!\"";
-
-// Display the escaped string
-window.alert(quoteMessage);
-
-// Section 3: Using document.write() to output to the page
-//document.write method
-document.write("For the last time, hello, world!");
-
-// Concatenate a string within document.write()
-document.write("JavaScript" + " is " + "awesome!");
-
-// Section 4: Concatenated String Challenge
-// Assign a concatenated string value to a variable and display it in the browser
-var concatenatedString = "Welcome" + " to " + "the " + "JavaScript " + "challenge!";
-document.write(concatenatedString);
-
-// Section 5: Multiple variable assignments
-// Assign multiple variables in one statement
-var num1 = 15, num2 = 25, num3 = 35;
-
-// Use document.write() to display one of the variables
-document.write(num1);
-
-// Section 6: Expressions with variables
-// Write an expression using the variables
-document.write(num1 + num2);
-
-// Section 7: Original example code
-//creating two variables
-var Sent1 = "This is the beginning of the string";
-var Sent2 = " and this is the end of the string";
-
-//displaying the variables using the document.write method
-document.write(Sent1 + Sent2);
-
-//an expression
-document.write(33 * 84);
-
-
-
-
-
-
+    // Use document.getElementById to display the concatenated string in the paragraph
+    var outputElement = document.getElementById('output');
+    outputElement.textContent = resultMessage;
+}
