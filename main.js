74a386dff5f522895f89d3d4a9ad49d0e3c48149
Abdalkabar <aabolghed@gmail.com>
2026-04-13 19:00:48 +0300

    project 3 math operators

--- main.js
+++ main.js
@@ -1,13 +1,49 @@
-function showCalculation() {
-    // Assign two variables
-    var number1 = 8;
-    var number2 = 12;
+// Set up two numbers for multiple math operations
+var firstNumber = 20;
+var secondNumber = 7;
 
-    // Build a message string using += to concatenate
-    var resultMessage = 'The sum of ' + number1 + ' and ' + number2 + ' is ';
-    resultMessage += (number1 + number2) + '.';
+// Subtraction
+var difference = firstNumber - secondNumber;
+document.write('The result of ' + firstNumber + ' - ' + secondNumber + ' is ' + difference + '.<br>');
 
-    // Use document.getElementById to display the concatenated string in the paragraph
-    var outputElement = document.getElementById('output');
-    outputElement.textContent = resultMessage;
-}
+// Multiplication
+var product = firstNumber * secondNumber;
+document.write('The result of ' + firstNumber + ' * ' + secondNumber + ' is ' + product + '.<br>');
+
+// Division
+var quotient = firstNumber / secondNumber;
+document.write('The result of ' + firstNumber + ' / ' + secondNumber + ' is ' + quotient + '.<br>');
+
+// Addition
+var sum = firstNumber + secondNumber;
+document.write('The result of ' + firstNumber + ' + ' + secondNumber + ' is ' + sum + '.<br>');
+
+// Modulus
+var remainder = firstNumber % secondNumber;
+document.write('The remainder of ' + firstNumber + ' % ' + secondNumber + ' is ' + remainder + '.<br>');
+
+// Negation
+var negatedNumber = -firstNumber;
+document.write('The negation of ' + firstNumber + ' is ' + negatedNumber + '.<br>');
+
+// Increment
+var incrementNumber = firstNumber;
+incrementNumber++;
+document.write('After incrementing, the value is ' + incrementNumber + '.<br>');
+
+// Decrement
+var decrementNumber = secondNumber;
+decrementNumber--;
+document.write('After decrementing, the value is ' + decrementNumber + '.<br>');
+
+// Random number
+var randomValue = Math.random();
+document.write('A random number between 0 and 1: ' + randomValue + '.<br>');
+
+// Math object method
+var sqrtValue = Math.sqrt(144);
+document.write('The square root of 144 using Math.sqrt() is ' + sqrtValue + '.<br>');
+
+// Combined operation
+var combined = firstNumber * secondNumber + (firstNumber - secondNumber);
+document.write('The result of ' + firstNumber + ' * ' + secondNumber + ' + (' + firstNumber + ' - ' + secondNumber + ') is ' + combined + '.');
